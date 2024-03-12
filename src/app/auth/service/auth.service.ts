523d69a3d365808c38249101e3f62add7b415958
Asura0107 <mircoorosio@gmail.com>
2024-03-12 19:26:49 +0100

    inseriti metodi di registrazione e login

---
+++ src/app/auth/service/auth.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { AuthData } from '../auth-data';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+import { BehaviorSubject, throwError, tap, catchError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  apiURL = environment.apiURL;
+  private authSubj = new BehaviorSubject<null | AuthData>(null);
+  user$ = this.authSubj.asObservable();
+  utente!: AuthData;
+  constructor(private http: HttpClient, private router: Router) {}
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+      errorMessage = error.error;
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      () => new Error('Something bad happened; please try again late.')
+    );
+  }
+
+  login(data: { email: string; password: string }) {
+    return this.http.post<AuthData>(`${this.apiURL}/auth/login`, data).pipe(
+      tap((loggato) => {
+        console.log(loggato);
+        this.authSubj.next(loggato);
+        this.utente = loggato;
+        console.log(this.utente);
+        localStorage.setItem('user', JSON.stringify(loggato));
+        console.log(this.user$);
+        this.router.navigate(['/']);
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  register(data: Partial<AuthData>) {
+    return this.http.post(`${this.apiURL}/auth/register`, data).pipe(
+      tap(() => {
+        this.router.navigate(['/login']);
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  logout() {
+    this.authSubj.next(null);
+    localStorage.removeItem('user');
+    this.router.navigate(['/']);
+  }
+}
